79c275322f925de8c37bfac1775ca2abbe409bfa
DongOk <hcan159@naver.com>
2023-11-25 20:00:14 +0900

    @add edit/delete task func

--- simple-todoapp/app/components/Task.tsx
+++ simple-todoapp/app/components/Task.tsx
@@ -1,15 +1,84 @@
+"use client";
+
 import { ITask } from "@/types/task";
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
+import { FiEdit, FiTrash, FiTrash2 } from "react-icons/fi";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setModalOpenEdit] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setTaskToEdit("");
+    setOpenModalDelete(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDelete(false);
+    router.refresh();
+  };
   return (
     <tr key={task.id}>
       <td>{task.text}</td>
-      <td>Blue</td>
+      <td className="flex gap-5">
+        <FiEdit
+          onClick={() => {
+            setModalOpenEdit(true);
+          }}
+          cursor="pointer"
+          classname="text-blue-500"
+          size={25}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setModalOpenEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit task</h3>
+            <div className="modal-action">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="inupt input-bordered w-full"
+              />
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2
+          onClick={() => setOpenModalDelete(true)}
+          cursor="pointer"
+          classname="text-red-500"
+          size={25}
+        />
+        <Modal modalOpen={openModalDelete} setModalOpen={setOpenModalDelete}>
+          <h3 className="text-lg">
+            Are you sure, you want to delete this task?
+          </h3>
+          <div className="modal-action">
+            <button onClick={() => handleDeleteTask(task.id)} className="btn">
+              Yes
+            </button>
+          </div>
+        </Modal>
+      </td>
     </tr>
   );
 };
