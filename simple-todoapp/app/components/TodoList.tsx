575462ed26c15618c7aee336c976564c13f915c9
DongOk <hcan159@naver.com>
2023-11-26 21:03:17 +0900

    @add table for view

--- simple-todoapp/app/components/TodoList.tsx
+++ simple-todoapp/app/components/TodoList.tsx
@@ -9,6 +9,12 @@ const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   console.log("tasks: ", tasks);
   return (
     <div className="overflow-x-auto">
+      <div>
+        <h3>경제 지표 뷰</h3>
+        <table className="table w-full">
+          <thead></thead>
+        </table>
+      </div>
       <table className="table w-full">
         <thead>
           <tr>
