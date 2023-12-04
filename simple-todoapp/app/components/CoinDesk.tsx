f14c403b106a740ab4b360d4340ec9608409122d
DongOk <hcan159@naver.com>
2023-12-04 16:24:05 +0900

    @add ethereum price

--- simple-todoapp/app/components/CoinDesk.tsx
+++ simple-todoapp/app/components/CoinDesk.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import coinmarketcap_raw from "../data/coinmarketcap_raw";
+
+interface CoinMarketCap_Price_Array {
+  symbol: string;
+  price: string;
+}
+
 async function CoinDesk() {
-  const BTC_Price = await coinmarketcap_raw();
-  return (
-    <div>
-      <h3>BTC/USDT : ${BTC_Price}</h3>
-    </div>
+  const [priceChange, setPriceChange] = useState<CoinMarketCap_Price_Array[]>(
+    []
   );
+  const Price_Array = await coinmarketcap_raw();
+  setPriceChange([]);
+  console.log(
+    "Price_Array",
+    Price_Array === undefined ? "" : Price_Array[0].btc
+  );
+  let btc_price: string;
+  let eth_price: string;
+  if (Price_Array !== undefined) {
+    btc_price = Price_Array[0].btc;
+    eth_price = Price_Array[0].eth;
+    return (
+      <div className=" m-auto, text-center">
+        <h3>BTC/USDT : ${btc_price}</h3>
+        <h3>ETH/USDT : ${eth_price}</h3>
+      </div>
+    );
+  }
 }
 
 export default CoinDesk;
--- simple-todoapp/app/data/coinmarketcap_raw.ts
+++ simple-todoapp/app/data/coinmarketcap_raw.ts
@@ -10,12 +10,24 @@ function coinmarketcap_raw() {
   //   throw new Error("Not Api key in CoinMarketCap");
   // }
   const raw_Data = async () => {
-    const rd = await axios.get(
+    const btc_raw = await axios.get(
       "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=bitcoin&start=1&limit=10&category=spot&centerType=all&sort=cmc_rank_advanced&direction=desc&spotUntracked=true"
     );
+    const eth_raw = await axios.get(
+      "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=ethereum&start=1&limit=10&category=spot&centerType=all&sort=cmc_rank_advanced&direction=desc&spotUntracked=true"
+    );
+
     // check Currency Symbol
-    if (rd.data.data.marketPairs[1].quoteSymbol === "USDT") {
-      return rd.data.data.marketPairs[1].price;
+    if (
+      btc_raw.data.data.marketPairs[1].quoteSymbol &&
+      eth_raw.data.data.marketPairs[0].quoteSymbol === "USDT"
+    ) {
+      return [
+        {
+          btc: btc_raw.data.data.marketPairs[1].price,
+          eth: eth_raw.data.data.marketPairs[0].price,
+        },
+      ];
     }
   };
   return raw_Data();
