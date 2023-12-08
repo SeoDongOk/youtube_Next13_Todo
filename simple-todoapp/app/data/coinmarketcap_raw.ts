8ca4fc999b476787fb35c3545b3446bd7dd3b9d7
DongOk <hcan159@naver.com>
2023-12-08 23:25:52 +0900

    Revert "@add ethereum price"
    This reverts commit f14c403b106a740ab4b360d4340ec9608409122d.
    
    # Conflicts:
    #	simple-todoapp/app/components/CoinDesk.tsx

--- simple-todoapp/app/data/coinmarketcap_raw.ts
+++ simple-todoapp/app/data/coinmarketcap_raw.ts
@@ -12,24 +12,12 @@ function coinmarketcap_raw() {
 
   //from CoinDesk Web
   const raw_Data = async () => {
-    const btc_raw = await axios.get(
+    const rd = await axios.get(
       "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=bitcoin&start=1&limit=10&category=spot&centerType=all&sort=cmc_rank_advanced&direction=desc&spotUntracked=true"
     );
-    const eth_raw = await axios.get(
-      "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=ethereum&start=1&limit=10&category=spot&centerType=all&sort=cmc_rank_advanced&direction=desc&spotUntracked=true"
-    );
-
     // check Currency Symbol
-    if (
-      btc_raw.data.data.marketPairs[1].quoteSymbol &&
-      eth_raw.data.data.marketPairs[0].quoteSymbol === "USDT"
-    ) {
-      return [
-        {
-          btc: btc_raw.data.data.marketPairs[1].price,
-          eth: eth_raw.data.data.marketPairs[0].price,
-        },
-      ];
+    if (rd.data.data.marketPairs[1].quoteSymbol === "USDT") {
+      return rd.data.data.marketPairs[1].price;
     }
   };
   return raw_Data();
